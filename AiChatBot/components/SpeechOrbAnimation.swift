f8a01096c5417e24879ad46453205e8338dc4696
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-12-01 05:09:38 +0100

    first commit

---
+++ AiChatBot/components/SpeechOrbAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  SpeechOrbAnimation.swift
+//  AiChatBot
+//
+//  Created by Godwin IE on 01/12/2023.
+//
+
+import SwiftUI
+
+struct SpeechOrbAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeechOrbAnimation()
+}
---
+++ AiChatBot/view/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  AiChatBot
+//
+//  Created by Godwin IE on 01/12/2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
