42f357eaeff5496401aa0b7a029b6cc7cce6bd26
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-12-01 03:32:37 +0100

    Initial Commit

---
+++ AiChatBot/AiChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatBotApp.swift
+//  AiChatBot
+//
+//  Created by Godwin IE on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AiChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
